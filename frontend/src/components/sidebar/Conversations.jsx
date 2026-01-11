e85fa1edc6331dc986069e4479fc182e9bd19967
Kr-Sahil-Works <sahillearn44@gmail.com>
2026-01-11 17:33:00 +0530

    ok

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,24 +1,29 @@
-import React from 'react'
-import Conversation from './Conversation.jsx'
-import useGetConversations from '../../hooks/useGetConversation.js';
-import {getRandomEmoji} from '../../utils/emoji.js';
+import React from "react";
+import Conversation from "./Conversation.jsx";
+import useGetConversations from "../../hooks/useGetConversation.js";
+import { getRandomEmoji } from "../../utils/emoji.js";
 
 const Conversations = () => {
-  const { loading , conversations } = useGetConversations();
- 
-  
-  return(
-    <div className='py-2 flex flex-col overflow-auto'>
-      {conversations.map((conversation,idx)=>(
+  const { loading, conversations } = useGetConversations();
+
+  const safeConversations = Array.isArray(conversations) ? conversations : [];
+
+  return (
+    <div className="py-2 flex flex-col overflow-auto">
+      {safeConversations.map((conversation, idx) => (
         <Conversation
           key={conversation._id}
           conversation={conversation}
           emoji={getRandomEmoji()}
-          lastIdx = {idx === conversations.length -1 }
+          lastIdx={idx === safeConversations.length - 1}
         />
       ))}
-       {loading ? <span className='loading loading-infinity mx-auto'></span> : null}
+
+      {loading && (
+        <span className="loading loading-infinity mx-auto"></span>
+      )}
     </div>
-  )
-}
-export default Conversations
+  );
+};
+
+export default Conversations;
